2598784da337dabe72bba216feb53771f9c0a7a3
hatout01 <hatout1@gmail.com>
2020-04-15 00:15:38 -0700

    moved some folders to right place

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+import Login from "./Components/Login/Login";
+//
+import Navbar from "./Components/Navbar/Navbar";
+import Register from "./Components/Register/Register";
+
+// import Footer from "../Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ client/src/Components/WatchList/WatchList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class WatchList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>WatchList</h1>
+      </div>
+    );
+  }
+}
